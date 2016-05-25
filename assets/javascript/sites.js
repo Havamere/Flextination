9c96eb9f0407556ddb37913a6f2f8e7ebd67f12c
jdurant0529 <james.durant@gmail.com>
2016-05-25 18:11:37 -0400

    nothing good came about, kyle to the rescue

--- assets/javascript/sites.js
+++ assets/javascript/sites.js
@@ -8,119 +8,83 @@ var markers = [];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: { lat: -34.397, lng: 150.644 },
         zoom: 9,
         heading: 90,
         tilt: 45,
-        styles: [ 
-        {
-            elementType: 'labels'
-            
-        },
+        styles: [{
+                elementType: 'labels'
 
- {
-        "featureType": "administrative",
-        "stylers": [
+            },
+
             {
-                "visibility": "off"
+                "featureType": "administrative",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "poi",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "water",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "transit",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "landscape",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "road.highway",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "road.local",
+                "stylers": [{
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [{
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "water",
+                "stylers": [{
+                    "color": "#abbaa4"
+                }]
+            }, {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#3f518c"
+                }]
+            }, {
+                "featureType": "road.highway",
+                "stylers": [{
+                    "color": "#ad9b8d"
+                }]
             }
         ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "color": "#abbaa4"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#3f518c"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "color": "#ad9b8d"
-            }
-        ]
-    }
-    ]
 
     }); //close map variable
-    
+
 
     infoWindow = new google.maps.InfoWindow();
     service = new google.maps.places.PlacesService(map);
@@ -158,34 +122,35 @@ function callback(results, status) {
 
 }
 
+
+
 function addMarker(place) {
 
-         // Try to get user physical location
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-              var pos = {
+    // Try to get user physical location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
-              };
+            };
 
-              infoWindow.setPosition(pos);
-              infoWindow.setContent('Location found.');
-              map.setCenter(pos);
-            }, function() {
-              handleLocationError(true, infoWindow, map.getCenter());
-            });
-          } 
-          else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-          }
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
-        }
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
 
 
     // The idle event is a debounced event, so we can query & listen without
@@ -193,100 +158,102 @@ function addMarker(place) {
     map.addListener('idle', performSearch);
 
     // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Binds the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-
-        
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-        return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
+    // Binds the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
     });
-    markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-            places.forEach(function(place) {
-              var icon = {
+
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            var icon = {
                 url: place.icon,
                 size: new google.maps.Size(71, 71),
                 origin: new google.maps.Point(0, 0),
                 anchor: new google.maps.Point(17, 34),
                 scaledSize: new google.maps.Size(25, 25)
-              };
+            };
 
 
-              // Create a marker for each place.
-              markers.push(new google.maps.Marker({
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
                 map: map,
                 icon: icon,
                 title: place.name,
                 position: place.geometry.location
-              }));
+            }));
 
-                   if (place.geometry.viewport) {
+            if (place.geometry.viewport) {
                 // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
-              } else {
+            } else {
                 bounds.extend(place.geometry.location);
-              }
-            });//Place for each closer
-    
-    map.fitBounds(bounds);
-  
- });//Searchbox listener closer
+            }
+        }); //Place for each closer
 
-}//Close initmap
+        map.fitBounds(bounds);
+
+    }); //Searchbox listener closer
+
+} //Close initmap
 
 //Pass the text value of the button as an argument to the performSearch function when user clicks the button
-$(".pass").on("click", function () {
-    console.log($(this).text());
-    var buttonSearch = $(this).text();
-    performSearch(buttonSearch);
-   
-})
-//Function to perform the search
+$(".pass").on("click", function() {
+        console.log($(this).text());
+        var buttonSearch = $(this).text();
+        performSearch(buttonSearch);
+
+    })
+    //Function to perform the search
 function performSearch(buttonSearch) {
 
-  for (var i = 0; i < markers.length; i++ ) {
-    markers[i].setMap(null);
-  }
-  markers.length = 0;
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers.length = 0;
 
     if (buttonSearch == null) {
-     buttonSearch = 'pizza';
-    }  
+        buttonSearch = 'pizza';
+    }
 
     var request = {
         bounds: map.getBounds(),
-        keyword: buttonSearch        
+        keyword: buttonSearch
     };
-//Radar search that returns a large list of places within specific radius
+    //Radar search that returns a large list of places within specific radius
     service.radarSearch(request, callback);
 }
 
 function callback(results, status) {
+    deleteMarkers();
     if (status !== google.maps.places.PlacesServiceStatus.OK) {
         console.error(status);
         return;
     }
-    for (var i = 0; i < results.length; i++) {
+    for (var i = 0; i < 5; i++) {
         addMarker(results[i]);
+        addResults(results[i]);
     }
 }
 
@@ -302,11 +269,12 @@ function addMarker(place) {
         }
     });
 
+
     //Event Listener thats executed when a marker is clicked to display place info
     google.maps.event.addListener(marker, 'click', function() {
         service.getDetails(place, function(result, status) {
             // console.log(result)
-            
+
             if (status !== google.maps.places.PlacesServiceStatus.OK) {
                 console.error(status);
                 return;
@@ -320,16 +288,26 @@ function addMarker(place) {
 
 
 function addResults(place) {
-    google.maps.event.addListener(marker, 'click', function() {
-                service.getDetails(place, function(result, status) {
-                    console.log(result);
-                    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                        console.error(status);
-                        return;
-                    }
-                    infoWindow.setContent("<strong>name: </strong>" + result.name + "<p>" + "<strong>address:  </strong>" + result.formatted_address + "<p>" + "<strong>phone number:  </strong>" + result.formatted_phone_number + "<p>" + "<strong>rating: </strong>" + result.rating + "<p>");
+    //google.maps.event.addListener(marker, 'click', function() {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        icon: {
+            url: 'http://maps.gstatic.com/mapfiles/circle.png',
+            anchor: new google.maps.Point(10, 10),
+            scaledSize: new google.maps.Size(10, 17)
+        }
+    });
+    service.getDetails(place, function(result, status) {
+        console.log(result);
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            console.error(status);
+            return;
+        }
+        $('#mapResults').append("<p><b>name:</b> " + result.name + "<p><b>address: </b>" + result.formatted_address + "<p><b>phone number:  </b>" + result.formatted_phone_number + "<p><b>rating: </b>" + result.rating + "<p>___________________________");
 
-                    infoWindow.open(map, marker);
-                });
-            })
-          }
+        // infoWindow.open(map, marker);
+    });
+    //    })
+}
+
