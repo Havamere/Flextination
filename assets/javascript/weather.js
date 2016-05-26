70f12436185f5cf46fb0b845cce0320838c07f5d
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-05-25 20:02:47 -0400

    working with unix time and adding in latitude+longitude

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -3,8 +3,12 @@
   'use strict';
 
   var destinationCity = 'Orlando';
+  var destinationLatitude = "41.85";
+  var destinationLongitude = "87.65";
   var weatherAPIkey = "524901&APPID=49d879c3e237943a90e1e4d5e68e9770";
-  var queryURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + destinationCity + "&units=imperial&cnt=16?id=" + weatherAPIkey;
+  // var queryURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + destinationCity + "&units=imperial&cnt=16?id=" + weatherAPIkey;
+  var queryURL = "http://api.openweathermap.org/data/2.5/history/city?lat=" + destinationLatitude + "&lon=" + destinationLongitude + "&type=day&start=" + (moment()) + "&end=" + (moment().date() + 16) + "&units=imperial&cnt=16?id=" + weatherAPIkey;
+
 
   $.ajax({
       url: queryURL,
@@ -26,6 +30,25 @@
           $('#destinationWeatherContent').append('<li>rain = ' + response.list[0].rain + '</li>');
           $('#destinationWeatherContent').append('<li>windspeed = ' + response.list[0].speed + 'mph</li>');
 
+          var selectedStartDateTime = "05/25/2016 9:00";
+          var unixSelectedStartDateTime = moment(moment(selectedStartDateTime, "MM/DD/YYYY HH:mm").unix()*1000);
+          console.log(unixSelectedStartDateTime);
+
+          var now = moment();
+          var tooFar = (now+(moment().date() + 16));
+          // var tooFar = true
+          // if ( >= unixSelectedStartDateTime) {
+          //   var unixSelectedStartDateTime = historicalStartDateTime;
+          //   var historicalStartDateTime = moment.unix(historicalStartDateTime);
+          //   var m = moment(historicalStartDateTime);
+          //   var s = m.format("M/D/YYYY H:mm");
+          //   console.log(s);
+          // } else {
+          //   return;
+          // }
+
+
+/*
           var time;
           var UNIX_timestamp = response.list[0].dt;
           function timeConverter(UNIX_timestamp) {
@@ -41,7 +64,7 @@
             return time;
           }
           $('#destinationWeatherContent').append('<p>time of day forecasted = ' + time + '</p>');
-
+*/
 
         });
     });
