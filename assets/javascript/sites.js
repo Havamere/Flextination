42c458ea376d43b5176d93cc9149662348210016
jdurant0529 <james.durant@gmail.com>
2016-05-25 20:28:05 -0400

    first to sencond page operational

--- assets/javascript/sites.js
+++ assets/javascript/sites.js
@@ -26,10 +26,11 @@ function fillInAddress() {
   // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
   console.log(place);
-  latlong = {lat: place.geometry.location.lat(),
-             lng: place.geometry.location.lng()
-         }
-    console.log(latlong);  
+  lat = place.geometry.location.lat();  
+  long  = place.geometry.location.lng()
+    
+    localStorage.setItem('lat',lat);
+    localStorage.setItem('long',long);   
 
   // Get each component of the address from the place details
   // and fill the corresponding field on the form.
@@ -54,317 +55,69 @@ function geolocate() {
   }
 }
 
+
+// function initMap() {
+//   var haightAshbury = {lat: 37.769, lng: -122.446};
+
 //Initialize the map
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: latlong,
-        zoom: 9,
-        heading: 90,
-        tilt: 45,
-        styles: [ 
-        {
-            elementType: 'labels'
-            
-        },
+    var lat = parseFloat(localStorage.getItem('lat'));
+    var long = parseFloat(localStorage.getItem('long'));
 
- {
-        "featureType": "administrative",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "color": "#abbaa4"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#3f518c"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "color": "#ad9b8d"
-            }
-        ]
-    }
-    ]
+   // var numIndex = lat.indexOf(".");
+   // lat = lat.substring(0, numIndex+4);
+   // numIndex = long.indexOf(".");
+   // long = long.substring(0,numIndex+4);
 
-    }); //close map variable
-    
+    var haightAshbury = {lat: lat, lng: long};
+        console.log(haightAshbury);
+    // console.log(lat, long);
 
-    infoWindow = new google.maps.InfoWindow();
-    service = new google.maps.places.PlacesService(map);
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
 
-    // The idle event is a debounced event, so we can query & listen without
-    // throwing too many requests at the server.
-    map.addListener('idle', performSearch);
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener('click', function(event) {
+    addMarker(event.latLng);
+  });
+
+  // Adds a marker at the center of the map.
+ addMarker(haightAshbury);
 }
 
-function performSearch() {
-    var request = {
-        bounds: map.getBounds(),
-        keyword: 'pizza',
-        limit: 5,
-
-    };
-
-    service.radarSearch(request, callback);
-
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
 }
 
-function callback(results, status) {
-
-
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        markerArr = [];
-        for (var i = 0; i < 5; i++) {
-            markerArr.push(results[i].geometry.location);
-            result = results[i];
-            addMarker(results[i]);
-        }
-    } else {
-        alert("Sorry, there are no locations in your area");
-    }
-
-}
-
-function addMarker(place) {
-
-         // Try to get user physical location
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-              var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-              };
-
-              infoWindow.setPosition(pos);
-              infoWindow.setContent('Location found.');
-              map.setCenter(pos);
-            }, function() {
-              handleLocationError(true, infoWindow, map.getCenter());
-            });
-          } 
-          else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-          }
-
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
-        }
-
-
-    // The idle event is a debounced event, so we can query & listen without
-    // throwing too many requests at the server.
-    map.addListener('idle', performSearch);
-
-    // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-        // Binds the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-
-        
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-        return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-            places.forEach(function(place) {
-              var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-              };
-
-
-              // Create a marker for each place.
-              markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-              }));
-
-                   if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-              } else {
-                bounds.extend(place.geometry.location);
-              }
-            });//Place for each closer
-    
-    map.fitBounds(bounds);
-  
- });//Searchbox listener closer
-
-}//Close initmap
-
-//Pass the text value of the button as an argument to the performSearch function when user clicks the button
-$(".pass").on("click", function () {
-    console.log($(this).text());
-    var buttonSearch = $(this).text();
-    performSearch(buttonSearch);
-   
-})
-//Function to perform the search
-function performSearch(buttonSearch) {
-
-  for (var i = 0; i < markers.length; i++ ) {
-    markers[i].setMap(null);
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
   }
-  markers.length = 0;
-
-    if (buttonSearch == null) {
-     buttonSearch = 'pizza';
-    }  
-
-    var request = {
-        bounds: map.getBounds(),
-        keyword: buttonSearch        
-    };
-//Radar search that returns a large list of places within specific radius
-    service.radarSearch(request, callback);
 }
 
-function callback(results, status) {
-    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        console.error(status);
-        return;
-    }
-    for (var i = 0; i < results.length; i++) {
-        addMarker(results[i]);
-    }
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
 }
 
-//Function that adds markers
-function addMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location,
-        icon: {
-            url: 'http://maps.gstatic.com/mapfiles/circle.png',
-            anchor: new google.maps.Point(10, 10),
-            scaledSize: new google.maps.Size(10, 17)
-        }
-    });
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
 
-    //Event Listener thats executed when a marker is clicked to display place info
-    google.maps.event.addListener(marker, 'click', function() {
-        service.getDetails(place, function(result, status) {
-            // console.log(result)
-            
-            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                console.error(status);
-                return;
-            }
-            infoWindow.setContent("<strong>name: </strong>" + result.name + "<p>" + "<strong>address:  </strong>" + result.formatted_address + "<p>" + "<strong>phone number:  </strong>" + result.formatted_phone_number + "<p>" + "<strong>rating: </strong>" + result.rating + "<p>");
-
-            infoWindow.open(map, marker);
-        });
-    });
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
 }
 
 
@@ -385,5 +138,8 @@ function addResults(place) {
 
 function moveToInfo(){
     console.log("Heyyy");
+    window.location.href = 'info.html';
+    
+    initMap();
 }
 $(document).on('click', '#next', moveToInfo);
