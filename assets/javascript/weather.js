596f7308a07260e9468d879a52b71023b1b6d4c6
jdurant0529 <james.durant@gmail.com>
2016-05-27 23:47:57 -0400

    added moment dates to html and weather.js

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -4,7 +4,13 @@
   var destinationCity = localStorage.getItem('address');
   var destinationLatitude = parseFloat(localStorage.getItem('lat'));
   var destinationLongitude = parseFloat(localStorage.getItem('long'));
-  $("#search-parameters").append(destinationCity);
+  var destinationStartDate = localStorage.getItem('startDate');
+  var destinationEndDate = localStorage.getItem('endDate');
+
+  var start = moment(destinationStartDate);
+  var end = moment(destinationEndDate);
+
+  $("#search-parameters").append(destinationCity + " between the dates of " + start.format('MM/DD/YYYY') + " and " + end.calendar('MM/DD/YYYY') + ".");
   var weatherAPIkey = "524901&APPID=49d879c3e237943a90e1e4d5e68e9770";
   // var queryURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + destinationCity + "&units=imperial&cnt=16?id=" + weatherAPIkey;
   // var queryURL = "http://api.openweathermap.org/data/2.5/history/city?lat=" + destinationLatitude + "&lon=" + destinationLongitude + "&type=hour&start=" + "1369728000" + "&end=" + "1369789200" + "&units=imperial?id=" + weatherAPIkey;
